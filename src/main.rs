5477f9e796ca5b6f3e97a61fe17b0c4f086b1618
Jack Jutzi <jsjutzi@gmail.com>
2022-10-14 19:28:16 -0400

    comments

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
+        //TODO: Expand to real structure
         let stream = stream.unwrap();
 
         println!("Connection established");
